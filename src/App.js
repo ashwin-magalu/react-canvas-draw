1703448ad102608138eea83e33d09544d73470fa
Ashwin M A <ashwin@gamorgan.in>
2021-02-19 16:28:38 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import TryOne from './components/pixi/TryOne';
+import Example from './components/roughjs/Example';
+import SampleOne from './components/canvas/SampleOne';
+import TryTwo from './components/pixi/TryTwo';
+
+function App() {
+  return (
+    <div className="app">
+      {/* <SampleOne /> */}
+      <Example />
+      {/* <TryOne /> */}
+      {/* <TryTwo /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pixi/TryTwo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TryTwo = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default TryTwo
